3e90770eb84ac665e7c4dd99d5c1ee86029743b6
Tom Busby <tom@flyfreeweb.co.uk>
2018-05-23 18:22:16 +0200

    EmailMessage class and proper date parsing (with edge cases)

--- parser.py
+++ parser.py
@@ -4,34 +4,53 @@ import re
 import pprint
 import sqlite3
 import dateutil.parser
-import warnings
-warnings.filterwarnings("error")
 
 
-timezone_info = {
-    'EST': -18000,
-    'EDT': -14400,
-    'CST': -21600,
-    'CDT': -18000,
-    'MST': -25200,
-    'MDT': -21600,
-    'PST': -28800,
-    'PDT': -25200,
-    'PPE': -25200,
-}
+class EmailMessage(object):
+
+    def __init__(self, file_year, raw_text):
+        self.file_year = file_year
+        self.raw_text = raw_text
+        parsed_message = Parser().parsestr(raw_text)
+        self.raw_date = parsed_message['Date']
+        self.parsed_date = self.parse_date_info(self.raw_date)
+        self.message_id = parsed_message['Message-ID'],
+        self.date = parsed_message['From'],
+        self.to = parsed_message['To'],
+        self.subject = parsed_message['Subject'],
+        self.reply_to = parsed_message['In-Reply-To']
+
+    def parse_date_info(self, raw_date):
+        tzinfos = {
+            'EST': -18000,
+            'EDT': -14400,
+            'CST': -21600,
+            'CDT': -18000,
+            'MST': -25200,
+            'MDT': -21600,
+            'PST': -28800,
+            'PPE': -28800,
+            'PDT': -25200,
+        }
+        try:
+            return dateutil.parser.parse(raw_date, tzinfos=tzinfos)
+        except ValueError:
+            found = re.search("SMTPSun, (.*) for karn", raw_date)
+            if found is not None:
+                return dateutil.parser.parse(found.group(1), tzinfos=tzinfos)
+            else:
+                return dateutil.parser.parse(raw_date.upper(), tzinfos=tzinfos)
 
 
 def get_messages():
     stringbuffer = ""
-    for n in range(2, 9):
-        with open("cryptome/cyp-199{}.txt".format(n)) as f:
-            i = 0
+    for n in range(1992, 1999):
+        with open("cryptome/cyp-{}.txt".format(n)) as f:
             for line in f:
                 if re.match("From cypherpunks\@MHonArc.venona", line):
                     if stringbuffer:
-                        yield Parser().parsestr(stringbuffer)
+                        yield EmailMessage(n, stringbuffer)
                     stringbuffer = ""
-                    i += 1
                 else:
                     stringbuffer += line
 
@@ -62,27 +81,9 @@ def insert_into_db(conn, message):
 
 def main():
     # conn = sqlite3.connect('database.db')
-    errors = []
-    warnings = []
-    timezones = []
     for message in get_messages():
-        # pprint.pprint(message.items())
         # insert_into_db(conn, message)
-        try:
-            parsed_date = dateutil.parser.parse(message['Date'], tzinfos=timezone_info)
-            if not parsed_date.tzinfo:
-                pprint.pprint((message['Date'], parsed_date))
-        except ValueError:
-            errors.append(message['Date'])
-        except dateutil.parser.UnknownTimezoneWarning as e:
-            timezone = re.match("tzname (\w+) identified", str(e)).group(1)
-            if timezone not in timezones:
-                timezones.append(timezone)
-            warnings.append(message['Date'])
-            # pprint.pprint((message['Date'], parsed_date))
-    print warnings
-    print errors
-    print timezones
+        pass
     # conn.close()
 
 
