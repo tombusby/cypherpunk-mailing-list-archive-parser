1d44f1344ab9a98f0396b73b7d3367ff66bab87a
Thomas Busby <tom@busby.ninja>
2018-06-20 01:56:15 +0200

    Generate thread page entirely

--- make_markdown_files.py
+++ make_markdown_files.py
@@ -1,7 +1,38 @@
 #!/usr/bin/env python
 import json
 from datetime import datetime
-from pprint import pprint
+
+
+file_header = """\
+---
+layout: default
+---
+
+# {} {}
+
+_Dates are calculated as the UTC date. The "raw date" from the email dump is included in brackets. The date may be inconsistent with the raw date because of the time difference with UTC time._
+
+_Ordering by UTC time ensures true chronological ordering._
+
+# Threads
+
+"""
+
+
+month_name_map = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+]
 
 
 def make_thread_list_item(message, offset, show_link=True):
@@ -9,38 +40,56 @@ def make_thread_list_item(message, offset, show_link=True):
         return "[{}](/years/{}/{}/{})".format(
             subject,
             parsed_date.year,
-            parsed_date.month,
+            str(parsed_date.month).zfill(2),
             message_hash
         )
     parsed_date = datetime.utcfromtimestamp(message['date'])
     if show_link:
-        subject = make_link(message['subject'], parsed_date, message['message_hash'])
+        subject = make_link(
+            message['subject'],
+            parsed_date,
+            message['message_hash']
+        )
     else:
         subject = message['subject']
     return "{}+ {}-{}-{} ({}) - {} - _{}_\n".format(
         "  " * offset,
         parsed_date.year,
-        parsed_date.month,
-        parsed_date.day,
+        str(parsed_date.month).zfill(2),
+        str(parsed_date.day).zfill(2),
         message['raw_date'],
         subject,
         message['from'].replace('<', '\\<').replace('>', '\\>')
     )
 
 
-def make_markdown_thread_tree(thread, message_hash, offset=0):
-    show_link = message_hash != thread['message_hash']
-    text = make_thread_list_item(thread, offset, show_link)
-    for child in thread['children']:
+def make_markdown_thread_tree(message, message_hash=None, offset=0):
+    show_link = message_hash != message['message_hash']
+    text = make_thread_list_item(message, offset, show_link)
+    for child in message['children']:
         text += make_markdown_thread_tree(child, message_hash, offset + 1)
     return text
+
+
+def make_markdown_thread(thread):
+    return "## {}\n{}".format(
+        thread['subject'],
+        make_markdown_thread_tree(thread)
+    )
 
 
 def main():
     with open('json_months/1992/9.json') as f:
         threads = json.loads(f.read())
-        thread = threads[3]
-        print make_markdown_thread_tree(thread, "17a09f8ed3e4c3e98e902c889e53518adfcf481d9d8ac1c147fbf572113bebf2")
+    with open('markdown_tests/09.md', 'w') as o:
+        parsed_date = datetime.utcfromtimestamp(threads[0]['date'])
+        o.write(file_header.format(
+            month_name_map[parsed_date.month - 1],
+            parsed_date.year
+        ))
+        for thread in threads:
+            o.write(make_markdown_thread(thread))
+            o.write("\n")
 
 
 if __name__ == "__main__":
