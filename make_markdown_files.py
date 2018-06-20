1894d4ff42b79c8c2fe6cb011031edd0e493ba96
Thomas Busby <tom@busby.ninja>
2018-06-20 03:14:34 +0200

    Produce pages for messages

--- make_markdown_files.py
+++ make_markdown_files.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import json
+import glob
+import os
 from datetime import datetime
 
 
@@ -10,12 +12,45 @@ layout: default
 
 # {} {}
 
-_Dates are calculated as the UTC date. The "raw date" from the email dump is included in brackets. The date may be inconsistent with the raw date because of the time difference with UTC time._
+_Dates are calculated as the UTC date. The "raw date" from \
+the email dump is included in brackets. The date may be inconsistent \
+with the raw date because of the time difference with UTC time._
 
 _Ordering by UTC time ensures true chronological ordering._
 
 # Threads
 
+"""
+
+
+message_page_template = """\
+---
+layout: default
+---
+
+# {} - {}
+
+## Header Data
+
+From: {}<br>
+To: {}<br>
+Message Hash: {}<br>
+Message ID: {}<br>
+Reply To: {}<br>
+UTC Datetime: {}<br>
+Raw Date: {}<br>
+
+## Raw message
+
+```
+{}
+```
+
+## Thread
+
+{}
+
+{}
 """
 
 
@@ -35,10 +70,56 @@ month_name_map = [
 ]
 
 
+def escape_chevrons(text):
+    if text:
+        return text.replace('<', '\\<').replace('>', '\\>')
+    else:
+        return "_N/A_"
+
+
+def create_message_pages(thread, message=None):
+    if not message:
+        message = thread
+    parsed_date = datetime.utcfromtimestamp(message['date'])
+    path = "emails_test/{}/{}/".format(
+        parsed_date.year,
+        str(parsed_date.month).zfill(2)
+    )
+    if not os.path.exists(path):
+        os.makedirs(path)
+    message_date = "{}-{}-{}".format(
+        parsed_date.year,
+        str(parsed_date.month).zfill(2),
+        str(parsed_date.day).zfill(2)
+    )
+    thread_tree = make_markdown_thread_tree(thread, message["message_hash"])
+    with open("raw_messages/{}/{}.txt".format(
+        message['file_year'], message["message_hash"]
+    )) as f:
+        raw_message = f.read()
+    with open("{}/{}.md".format(path, message["message_hash"]), "w") as o:
+        o.write(message_page_template.format(
+            message_date,
+            message["subject"].encode('utf-8'),
+            message["from"].encode('utf-8'),
+            escape_chevrons(message["to"]),
+            message["message_hash"],
+            escape_chevrons(message["message_id"]),
+            escape_chevrons(message["reply_to"]),
+            "_UTC DATETIME TODO_",
+            message["raw_date"],
+            raw_message,
+            "_BACK TO TODO_",
+            thread_tree
+        ))
+    for child in message["children"]:
+        create_message_pages(thread, child)
+
+
 def make_thread_list_item(message, offset, show_link=True):
     def make_link(subject, parsed_date, message_hash):
         return "[{}](/years/{}/{}/{})".format(
-            subject,
+            subject.encode('utf-8'),
             parsed_date.year,
             str(parsed_date.month).zfill(2),
             message_hash
@@ -59,7 +140,7 @@ def make_thread_list_item(message, offset, show_link=True):
         str(parsed_date.day).zfill(2),
         message['raw_date'],
         subject,
-        message['from'].replace('<', '\\<').replace('>', '\\>')
+        message['from'].encode('utf-8').replace('<', '\\<').replace('>', '\\>')
     )
 
 
@@ -72,24 +153,34 @@ def make_markdown_thread_tree(message, message_hash=None, offset=0):
 
 
 def make_markdown_thread(thread):
+    create_message_pages(thread)
     return "## {}\n{}".format(
-        thread['subject'],
+        thread['subject'].encode('utf-8'),
         make_markdown_thread_tree(thread)
     )
 
 
 def main():
-    with open('json_months/1992/9.json') as f:
-        threads = json.loads(f.read())
-    with open('markdown_tests/09.md', 'w') as o:
+    # for filename in glob.glob('json_months/199*/*.json'):
+    for filename in glob.glob('json_months/1992/*.json'):
+        if "unknown" in filename:
+            continue  # ############### HANDLE THIS LATER
+        with open(filename) as f:
+            threads = json.loads(f.read())
         parsed_date = datetime.utcfromtimestamp(threads[0]['date'])
-        o.write(file_header.format(
-            month_name_map[parsed_date.month - 1],
-            parsed_date.year
-        ))
-        for thread in threads:
-            o.write(make_markdown_thread(thread))
-            o.write("\n")
+        if not os.path.exists("threads_test/{}/".format(parsed_date.year)):
+            os.makedirs("threads_test/{}/".format(parsed_date.year))
+        with open('threads_test/{}/{}.md'.format(
+            parsed_date.year,
+            str(parsed_date.month).zfill(2)
+        ), 'w') as o:
+            o.write(file_header.format(
+                month_name_map[parsed_date.month - 1],
+                parsed_date.year
+            ))
+            for thread in threads:
+                o.write(make_markdown_thread(thread))
+                o.write("\n")
 
 
 if __name__ == "__main__":
